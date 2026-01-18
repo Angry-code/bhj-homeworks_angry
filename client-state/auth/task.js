a30188578597fae2c70da92d7df9f241add8fe15
Angry code <salhino@bk.ru>
2026-01-18 20:38:11 +0300

    Доработка задания auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,32 +1,43 @@
-const formAuth = document.getElementById('signin');
+const formAuth = document.getElementById('signin__form');
 const userTrue = document.getElementById('welcome');
-const 
+const userId = document.getElementById('user_id');
+const formSign = document.getElementById('signin')
+
+if (localStorage.getItem('user_id')) {
+    userTrue.classList.add('welcome_active');
+    userId.textContent = localStorage.getItem('user_id');
+
+}
 
 formAuth.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-
-    const formData = new FormData();
-
+    xhr.responseType = 'json'
+    const formData = new FormData(formAuth);
 
 
     xhr.onload = () => {
         const responses = xhr.response;
-        ;
-        if (responses.succes) {
-            formAuth.classList.remove('signin_active');
+        if (responses.success) {
+            userId.textContent = responses.user_id;
+            formSign.classList.remove('signin_active');
             userTrue.classList.add('welcome_active');
-            userTrue.innerHTML = `Добро пожаловать, пользователь #<span id="user_id">${id}</span>`;
-            localStorage.user = responses.user_id;
+            localStorage.setItem('user_id', responses.user_id);
+
         } else {
             alert("Неверный логин/пароль")
         };
     };
+
     xhr.send(formData);
     formAuth.reset();
+
 })
+
+
+
 
 
 
